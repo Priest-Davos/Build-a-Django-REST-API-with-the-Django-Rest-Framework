2f9aef3906a63f6a88b06be818bbec14c3173b19
priest davos <rajjo1920london@gmail.com>
2024-05-04 05:07:42 +0530

    till (00:31:00) seecomplete commit msg for detail
    create a requirement.txt file
    there write some pacages which we will install
       -  django
       -  djangorestframework
       -  pyyaml
       -  requests
       -  django-cors-headers
    
    then in terminal write -> pip install -r requirement.txt
    it will install all the mentioned libraries in requirement.txt file
    
    ___________________________________________________________________
     Now in current directory create two folder
      1. backened     - here will create  the project
      2. py_client    - will use the backened here to test
    
    _______________________________________________________________________
    
    Now cd to backened folder
    
    Now  in terminal
        -> django-admin startproject backend .
    
        here  . will create project in current directorty instead of creating a new directory
        ---it will create a folder name backend and a file manage.py
        ---to see tree , in terminal write -> tree /F
    
          C:.
          │   manage.py
          │
          └───backend
                  asgi.py
                  settings.py
                  urls.py
                  wsgi.py
                  __init__.py
    
    _______________________________________________________________________
    
     --> Now create a python API  Client (00:11:41)
    
    In py__client folder create a base.py file
    
    ------study about endpoints
    ------study about HTTP request  vs REST API HTTP request
    
    in base.py
        code:-
    
            import requests
    
            endpoint='https://httpbin.org/anything'
    
            response=requests.get(endpoint)
            print(response)# output= <Response [200]>
            print(response.headers)# {'Date': 'Fri, 03 May 2024 22:36:02 GMT', 'Content-Type': 'application/json', 'Content-Length': '394', 'Connection': 'keep-alive', 'Server': 'gunicorn/19.9.0', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': 'true'}
            print(response.encoding)# utf-8
    
            print (response.text) # return a json object
    
    _______________________________________________________________________________________
    
    Run Django Project  (00:27:03)
    
    Headover to terminal cd to backend and write-> python manage.py runserver
    
    Now it is up and running at port 8000 by default ie at http://127.0.0.1:8000/
    
    Now in base.py update the endpoint with this link
    endpoint='http://127.0.0.1:8000/'

---
+++ py_client/base.py
@@ -0,0 +1,31 @@
+import requests
+
+# endpoint='https://httpbin.org/anything'
+base_url='http://127.0.0.1:8000/'#or 'localhost:8000'
+endpoint=base_url
+
+
+response=requests.get(endpoint)
+print(response)# output= <Response [200]>
+print(response.headers)# {'Date': 'Fri, 03 May 2024 22:36:02 GMT', 'Content-Type': 'application/json', 'Content-Length': '394', 'Connection': 'keep-alive', 'Server': 'gunicorn/19.9.0', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': 'true'}
+print(response.encoding)# utf-8
+
+print (response.text) 
+# {
+#   "args": {},
+#   "data": "",
+#   "files": {},
+#   "form": {},
+#   "headers": {
+#     "Accept": "*/*",
+#     "Accept-Encoding": "gzip, deflate",
+#     "Host": "httpbin.org",
+#     "User-Agent": "python-requests/2.31.0",
+#     "X-Amzn-Trace-Id": "Root=1-66356751-7ac14030318f852e43df6889"
+#   },
+#   "json": null,
+#   "method": "GET",
+#   "origin": "42.108.27.76",
+#   "url": "https://httpbin.org/anything"
+# }
+print(response.json())
