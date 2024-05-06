5ee40fee7f0e48d7cb9739355f3f176707c85e45
priest davos <rajjo1920london@gmail.com>
2024-05-06 07:28:38 +0530

    Rest Framework View and Response (1:04:00) _______________________________________
    convert api_home view into Django rest Framework  View (DRF API view)
    
    update views.py file ... in it api_home view
    
     code :-
            from  rest_framework.response import Response
            from rest_framework.decorators import api_view
    
         ✔️ @api_view(['GET'])  #add it   ... can specify more methods like POST ,  etc
            def api_home(request, *args, **kwargs):
              model_data=Product.objects.all()
              data={}
              # data=[]
              if model_data:
                for product in model_data:
                    product_data=model_to_dict(product)
                    # product_data=model_to_dict(product, fields=['price']) # can also specify filds which we want to include
                    # data.append(product_data)
                    data[product.id]=product_data
    
                # data=json.dumps(data)
          ❌   # return JsonResponse(data,safe=False)  # remove it
          ✔️  return Response(data)   #add rest_Framework Response
     remove comments from base.py as this is new version so no need to keep old code
     run base.py to test updated view
     dont forget to run server
    ----------------------------------------------------------
    ✔️commit -6 till (1:04:00)  .. commit 1 on v3 branch
    ------------------------------------------------------

--- backened/api/views.py
+++ backened/api/views.py
@@ -1,53 +1,24 @@
-from django.http import JsonResponse
+# from django.http import JsonResponse
 import json
-# Create your views here.
-
-# till(00:47:00)
-# def api_home(request , *args , **kwargs ):
-#   # print(request) # <WSGIRequest: GET '/api/?user=1'>
-#   # print(type(request)) # <class 'django.core.handlers.wsgi.WSGIRequest'>
-#   # print(request.headers)#{'Content-Length': '25', 'Content-Type': 'application/json', 'Host': '127.0.0.1:8000', 'User-Agent': 'python-requests/2.31.0', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive'}
-#   # print(request.GET) #<QueryDict: {'user': ['1']}>
-#   # print(request.user) #AnonymousUser
-#   data={}
-#   if request.body:
-#      # in Django cannt do directly request.jsonn() unlike in Flask so  do json.loads(request.body)
-#       body=request.body
-#       # print(type(body)) #<class 'bytes'>
-#       data=json.loads(body)     
-#       print(data)#{'query': 'Helloo there'}
-#       print(data.keys()) #dict_keys(['query'])
-#       # data['headers']=request.headers # cannt do so chage it to dict
-#       print(type(request.headers)) #<class 'django.http.request.HttpHeaders'>
-#       data['headers']=dict(request.headers) # like this
-#       print(type(data)) #<class 'django.http.request.HttpHeaders'>
-#       print (data)
-#   # response={"message": " is the api_home view","msg2": "ok"}
-#   # return JsonResponse (response )
-#   return JsonResponse (data )
-
 
 from products.models import Product
 from django .forms.models import model_to_dict
 
+from  rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])  #add it   ... can specify more methods like POST ,  etc
 def api_home(request, *args, **kwargs):
   model_data=Product.objects.all()
   data={}
   # data=[]
   if model_data:
     for product in model_data:
-        # product_data = {
-        #     'id': product.id,
-        #     'title': product.title,
-        #     'content': product.content,  # Corrected attribute name
-        #     'price':str( product.price )
-        # }
-        # print(type(product))#<class 'products.models.Product'>
         product_data=model_to_dict(product)
         # product_data=model_to_dict(product, fields=['price']) # can also specify filds which we want to include 
         # data.append(product_data)
         data[product.id]=product_data
 
     # data=json.dumps(data)
-  
-  return JsonResponse(data,safe=False)
+  # return JsonResponse(data,safe=False)  # remove it
+  return Response(data)   #add rest_Framework Response
