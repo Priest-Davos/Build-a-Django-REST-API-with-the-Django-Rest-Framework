bec9e6b6a1de5da56a1b6a8e9d51105ec8c35423
priest davos <rajjo1920london@gmail.com>
2024-05-10 21:58:28 +0530

    Injest Data with DRF views (1:14:46)-(1:17:56) summarized the process of updating your Django views to handle both GET and POST requests using Django REST Framework (DRF) views. Here's a breakdown of the steps you've outlined:
    Update settings.py:
    Add "rest_framework" to the INSTALLED_APPS setting to enable Django REST Framework in your project.
    
    Update api_home view to handle GET and POST methods:
    Decorate the api_home view function with @api_view(['GET', 'POST']) to specify that it can handle both GET and POST requests.
    Inside the view function, check the request method using request.method.
    If the method is GET, retrieve data from the database, serialize it using either model_to_dict or a DRF serializer (ProductSerializer), and return the serialized data as a response using Response(data).
    If the method is POST, extract the data from the request using request.data, process it as needed, and return the response using Response(data).
    
    Testing the POST method:
    You can test the POST method by sending a POST request to the API endpoint (http://127.0.0.1:8000/api/) containing the data you want to add in the request body.
    Alternatively, you can use Python requests library to send a POST request programmatically. In your example, you're sending a POST request with JSON data ({"name": "Priest"}) to the API endpoint and printing the response.
    
    Optional Step - Testing with Python requests:
    You've provided code to test the POST method using Python requests library. After sending the POST request, you print the JSON response to verify that the data was successfully added.
    Overall, these steps demonstrate how to update your Django views to handle both GET and POST requests, allowing you to retrieve existing data and add new data via your API endpoint. This is a fundamental aspect of building RESTful APIs with Django REST Framework.

--- py_client/base.py
+++ py_client/base.py
@@ -5,28 +5,9 @@ base_url='http://127.0.0.1:8000/'#or 'localhost:8000'
 endpoint=base_url+"/api/"
 
 
-response=requests.get(endpoint,params={"user":1}, json= {"query":"Helloo there"})
-# print(response)# output= <Response [200]>
-# print(response.headers)# {'Date': 'Fri, 03 May 2024 22:36:02 GMT', 'Content-Type': 'application/json', 'Content-Length': '394', 'Connection': 'keep-alive', 'Server': 'gunicorn/19.9.0', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': 'true'}
-# print(response.encoding)# utf-8
+# response=requests.get(endpoint,params={"user":1}, json= {"query":"Helloo there"})
+# print(response.json())
 
-# print (response.text) 
-# {
-#   "args": {},
-#   "data": "",
-#   "files": {},
-#   "form": {},
-#   "headers": {
-#     "Accept": "*/*",
-#     "Accept-Encoding": "gzip, deflate",
-#     "Host": "httpbin.org",
-#     "User-Agent": "python-requests/2.31.0",
-#     "X-Amzn-Trace-Id": "Root=1-66356751-7ac14030318f852e43df6889"
-#   },
-#   "json": null,
-#   "method": "GET",
-#   "origin": "42.108.27.76",
-#   "url": "https://httpbin.org/anything"
-# }
-print(response.json())
+post_response=requests.post(endpoint, json={"name":"Priest" })
+print(post_response.json() )
 
